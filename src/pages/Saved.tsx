0e66e46273533d51d732d29bfd34dac072225b5c
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-01-14 02:21:11 +0600

    saved page added

--- src/pages/Saved.tsx
+++ src/pages/Saved.tsx
@@ -1,5 +1,57 @@
+import PostCard from "../common/components/PostCard";
+import iconList from "../common/lib/iconList";
+
 const Saved = () => {
-  return <div>Saved</div>;
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <h3 className="text-lg font-medium">Saved Posts</h3>
+
+        <button className="btn btn-sm btn-theme">
+          remove all <span>{iconList.remove}</span>
+        </button>
+      </div>
+
+      <div className="flex flex-col gap-3 mt-5">
+        <PostCard
+          id="1"
+          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, impedit"
+          createdAt={new Date().toISOString()}
+          modifiedAt={new Date().toISOString()}
+          likes={["tonmoy", "kdsa"]}
+          author={{
+            name: "Tonmoy Deb",
+            avatar: "/images/logo/chirkutt-logo-primary.png",
+            username: "tonmoydeb",
+          }}
+        />
+        <PostCard
+          id="1"
+          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, impedit"
+          createdAt={new Date().toISOString()}
+          modifiedAt={new Date().toISOString()}
+          likes={["tonmoy", "kdsa"]}
+          author={{
+            name: "Tonmoy Deb",
+            avatar: "/images/logo/chirkutt-logo-primary.png",
+            username: "tonmoydeb",
+          }}
+        />
+        <PostCard
+          id="1"
+          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, impedit"
+          createdAt={new Date().toISOString()}
+          modifiedAt={new Date().toISOString()}
+          likes={["tonmoy", "kdsa"]}
+          author={{
+            name: "Tonmoy Deb",
+            avatar: "/images/logo/chirkutt-logo-primary.png",
+            username: "tonmoydeb",
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Saved;
