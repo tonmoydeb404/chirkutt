34038566dd5477c9a808ec03b889392fea680553
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-02-02 14:23:19 +0600

    realtime updates removed

--- src/services/postsApi.ts
+++ src/services/postsApi.ts
@@ -1,79 +1,54 @@
 import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
 import { POSTS } from "../constants/firebase.constant";
-import {
-    createDocument,
-    getCollection,
-    getCollectionRealtime,
-} from "../lib/database";
+import { createDocument, getCollection } from "../lib/database";
 import { PostType } from "../types/PostType";
 import { arrayToObject } from "../utilities/arrayToObject";
 
 export const postsApi = createApi({
-    reducerPath: "postsApi",
-    baseQuery: fakeBaseQuery(),
-    endpoints: (builder) => ({
-        getAllPosts: builder.query({
-            queryFn: async () => {
-                try {
-                    const response = await getCollection<PostType>(POSTS);
-                    // sorting by time
-                    const sortedResponse = response.sort((a, b) => {
-                        return (
-                            new Date(b.createdAt).getTime() -
-                            new Date(a.createdAt).getTime()
-                        );
-                    });
-                    const data = arrayToObject(sortedResponse, "id");
-                    return { data };
-                } catch (error) {
-                    return { error };
-                }
-            },
-            onCacheEntryAdded: async (
-                arg,
-                { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
-            ) => {
-                let unsubscribe = () => {};
-                try {
-                    await cacheDataLoaded;
-                    unsubscribe = getCollectionRealtime<PostType>(
-                        POSTS,
-                        (data) => {
-                            updateCachedData((draft) => {
-                                // sorting by time
-                                const sortedResponse = data.sort((a, b) => {
-                                    return (
-                                        new Date(b.createdAt).getTime() -
-                                        new Date(a.createdAt).getTime()
-                                    );
-                                });
-                                draft = arrayToObject<PostType>(
-                                    sortedResponse,
-                                    "id"
-                                );
-                            });
-                        }
-                    );
-                } catch (error) {}
-                await cacheEntryRemoved;
-                unsubscribe();
-            },
-        }),
-        createPost: builder.mutation({
-            queryFn: async (params: PostType) => {
-                try {
-                    const response = await createDocument(
-                        params.id,
-                        POSTS,
-                        params
-                    );
-                    return { data: response };
-                } catch (error) {
-                    return { error };
-                }
-            },
-        }),
+  reducerPath: "postsApi",
+  baseQuery: fakeBaseQuery(),
+  tagTypes: ["Post"],
+  endpoints: (builder) => ({
+    getAllPosts: builder.query({
+      queryFn: async () => {
+        try {
+          const response = await getCollection<PostType>(POSTS);
+          // sorting by time
+          const sortedResponse = response.sort((a, b) => {
+            return (
+              new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+            );
+          });
+          const data = arrayToObject(sortedResponse, "id");
+          return { data };
+        } catch (error) {
+          return { error };
+        }
+      },
+      providesTags: (result) => {
+        return result
+          ? [
+              ...Object.keys(result).map((id) => ({
+                type: "Post" as const,
+                id,
+              })),
+              "Post",
+            ]
+          : ["Post"];
+      },
     }),
+    createPost: builder.mutation({
+      queryFn: async (params: PostType) => {
+        try {
+          const response = await createDocument(params.id, POSTS, params);
+          return { data: response };
+        } catch (error) {
+          return { error };
+        }
+      },
+      invalidatesTags: ["Post"],
+    }),
+  }),
 });
 
 // hooks
--- src/services/usersApi.ts
+++ src/services/usersApi.ts
@@ -1,94 +1,80 @@
 import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
 import { USERS } from "../constants/firebase.constant";
-import {
-    getCollection,
-    getCollectionRealtime,
-    getQueryResult,
-    updateDocument,
-} from "../lib/database";
+import { getCollection, getQueryResult, updateDocument } from "../lib/database";
 import { UserType } from "../types/UserType";
 import { arrayToObject } from "../utilities/arrayToObject";
 
 export const usersApi = createApi({
-    reducerPath: "usersApi",
-    baseQuery: fakeBaseQuery(),
-    endpoints: (builder) => ({
-        getAllUsers: builder.query({
-            queryFn: async () => {
-                try {
-                    const response = await getCollection<UserType>(USERS);
-                    const data = arrayToObject(response, "uid");
-                    return { data };
-                } catch (error) {
-                    return { error };
-                }
-            },
-            onCacheEntryAdded: async (
-                arg,
-                { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
-            ) => {
-                let unsubscribe = () => {};
-                try {
-                    await cacheDataLoaded;
-                    unsubscribe = getCollectionRealtime<UserType>(
-                        USERS,
-                        (data) => {
-                            updateCachedData(
-                                (draft) =>
-                                    (draft = arrayToObject<UserType>(
-                                        data,
-                                        "uid"
-                                    ))
-                            );
-                        }
-                    );
-                } catch (error) {}
-                await cacheEntryRemoved;
-                unsubscribe();
-            },
-        }),
-        getUser: builder.query({
-            queryFn: async ({ username }: { username: string }) => {
-                try {
-                    const response = (await getQueryResult<UserType>(
-                        [{ key: "username", value: username, condition: "==" }],
-                        USERS
-                    )) as UserType[];
-
-                    if (!response.length) throw "user not found";
-
-                    return { data: response[0] };
-                } catch (error) {
-                    return { error };
-                }
-            },
-        }),
-        updateUser: builder.mutation({
-            queryFn: async ({
-                uid,
-                updates,
-            }: {
-                uid: string;
-                updates: { [key: string]: any };
-            }) => {
-                try {
-                    const response = (await updateDocument(
-                        uid,
-                        USERS,
-                        updates
-                    )) as UserType;
-
-                    if (!response) throw "something went to wrong";
-
-                    return { data: response };
-                } catch (error) {
-                    return { error };
-                }
-            },
-        }),
+  reducerPath: "usersApi",
+  baseQuery: fakeBaseQuery(),
+  tagTypes: ["User"],
+  endpoints: (builder) => ({
+    getAllUsers: builder.query({
+      queryFn: async () => {
+        try {
+          const response = await getCollection<UserType>(USERS);
+          const data = arrayToObject(response, "uid");
+          return { data };
+        } catch (error) {
+          return { error };
+        }
+      },
+      providesTags: (result) => {
+        return result
+          ? [
+              ...Object.keys(result).map((id) => ({
+                type: "User" as const,
+                id,
+              })),
+              "User",
+            ]
+          : ["User"];
+      },
     }),
+    getUser: builder.query({
+      queryFn: async ({ username }: { username: string }) => {
+        try {
+          const response = (await getQueryResult<UserType>(
+            [{ key: "username", value: username, condition: "==" }],
+            USERS
+          )) as UserType[];
+
+          if (!response.length) throw "user not found";
+
+          return { data: response[0] };
+        } catch (error) {
+          return { error };
+        }
+      },
+      providesTags: ["User"],
+    }),
+    updateUser: builder.mutation({
+      queryFn: async ({
+        uid,
+        updates,
+      }: {
+        uid: string;
+        updates: { [key: string]: any };
+      }) => {
+        try {
+          const response = (await updateDocument(
+            uid,
+            USERS,
+            updates
+          )) as UserType;
+
+          if (!response) throw "something went to wrong";
+
+          return { data: response };
+        } catch (error) {
+          return { error };
+        }
+      },
+      invalidatesTags: ["User"],
+    }),
+  }),
 });
 
 // hooks
 export const { useGetUserQuery, useUpdateUserMutation, useGetAllUsersQuery } =
-    usersApi;
+  usersApi;
