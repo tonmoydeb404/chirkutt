b6b0fe30b2cf85000da122a185a2620a8c7c93e8
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-02-12 23:17:35 +0600

    search page updated

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -3,7 +3,7 @@ import { Navigate, useSearchParams } from "react-router-dom";
 import PostCard from "../common/components/PostCard";
 import { useAuth } from "../common/outlet/PrivateOutlet";
 import { useGetAllCommentsQuery } from "../services/commentsApi";
-import { useLazySearchPostsQuery } from "../services/postsApi";
+import { useGetAllPostsQuery } from "../services/postsApi";
 import { useLazyGetSavedPostsQuery } from "../services/savedApi";
 import { useGetAllUsersQuery } from "../services/usersApi";
 
@@ -14,25 +14,10 @@ const Search = () => {
   if (!query) return <Navigate to={"/"} />;
 
   const auth = useAuth();
-  const [getSearchPosts, posts] = useLazySearchPostsQuery();
+  const posts = useGetAllPostsQuery();
   const users = useGetAllUsersQuery({});
   const comments = useGetAllCommentsQuery({});
   const [getSavedPost, savedPostResult] = useLazyGetSavedPostsQuery();
-
-  // trigger get query posts
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        if (query) {
-          await getSearchPosts(query).unwrap();
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    fetchPost();
-  }, [query]);
 
   // trigger get saved post
   useEffect(() => {
@@ -64,23 +49,29 @@ const Search = () => {
     return (
       <div className="flex flex-col gap-3">
         {Object.keys(posts.data).length
-          ? Object.keys(posts.data).map((key: string) => {
-              const post = posts.data[key];
-              const author = users.data[post.authorUID];
-              const postComments = Object.keys(comments.data).filter(
-                (c) => comments.data[c].postID === post.id
-              );
-              const isSaved = !!savedPostResult?.data?.[post.id];
-              return (
-                <PostCard
-                  key={post.id}
-                  {...post}
-                  author={author}
-                  comments={postComments.length}
-                  isSaved={isSaved}
-                />
-              );
-            })
+          ? Object.keys(posts.data)
+              .filter((key) => {
+                const post = posts.data[key];
+                if (!post) return false;
+                return post.text.toLowerCase().includes(query.toLowerCase());
+              })
+              .map((key: string) => {
+                const post = posts.data[key];
+                const author = users.data[post.authorUID];
+                const postComments = Object.keys(comments.data).filter(
+                  (c) => comments.data[c].postID === post.id
+                );
+                const isSaved = !!savedPostResult?.data?.[post.id];
+                return (
+                  <PostCard
+                    key={post.id}
+                    {...post}
+                    author={author}
+                    comments={postComments.length}
+                    isSaved={isSaved}
+                  />
+                );
+              })
           : "cannot find any posts"}
       </div>
     );
