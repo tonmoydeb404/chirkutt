043a24c058fb2291330b4d0830c773703a0ae956
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-04-01 00:39:19 +0600

    mobile dropdown added

--- src/common/layout/MobileMenu.tsx
+++ src/common/layout/MobileMenu.tsx
@@ -2,10 +2,68 @@
 import { NavLink } from "react-router-dom";
 import { useAppDispatch } from "../../app/hooks";
 import { createPostModal } from "../../features/postModal/postModalSlice";
-import { signout } from "../../lib/auth";
 import iconList from "../../lib/iconList";
 import { AuthType } from "../../types/AuthType";
 import { ListItemType } from "../../types/ListType";
+
+import { Popover, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { authSignOut } from "../../features/auth/authSlice";
+
+function DropDownMenu({ list }: { list: ListItemType[] }) {
+  return (
+    <Popover as="div">
+      <Popover.Button
+        className={`inline-block text-2xl py-2 px-2 hover:bg-neutral-200 dark:hover:bg-neutral-700 rounded`}
+      >
+        {iconList.menu}
+      </Popover.Button>
+      <Popover.Overlay className="fixed inset-0 bg-black opacity-40" />
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Popover.Panel className="absolute z-10 bottom-full mb-0 w-full left-0 right-0">
+          {({ close }) => (
+            <div className="flex flex-col box px-2 py-4 gap-1">
+              {list.map((link) =>
+                "path" in link ? (
+                  <Link
+                    key={link.title}
+                    onClick={() => close()}
+                    to={link.path}
+                    className="px-3 py-2 border border-neutral-700 rounded flex items-center gap-1 hover:bg-neutral-200 dark:hover:bg-neutral-700"
+                  >
+                    <span className="text-lg">{iconList[link.icon]}</span>
+                    <span>{link.title}</span>
+                  </Link>
+                ) : (
+                  <button
+                    key={link.title}
+                    onClick={() => {
+                      close();
+                      link.action();
+                    }}
+                    className="w-full px-3 py-2 border border-neutral-700 rounded flex items-center gap-1 hover:bg-neutral-200 dark:hover:bg-neutral-700"
+                  >
+                    <span className="text-lg">{iconList[link.icon]}</span>
+                    <span>{link.title}</span>
+                  </button>
+                )
+              )}
+            </div>
+          )}
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  );
+}
 
 const MobileMenu = ({
   notifications,
@@ -18,24 +76,37 @@ const MobileMenu = ({
 
   const authorizedLinks: ListItemType[] = [
     { title: "Home", path: "/", icon: "home" },
-    { title: "Saved", path: "/saved", icon: "bookmarks" },
-    {
-      title: "New post",
-      action: () => dispatch(createPostModal()),
-      icon: "add",
-    },
     {
       title: "Notifications",
       path: "/notifications",
       icon: "notification",
       badge: notifications ? "WARNING" : undefined,
     },
+    {
+      title: "New post",
+      action: () => dispatch(createPostModal()),
+      icon: "add",
+    },
     { title: "Profile", path: "/profile", icon: "person" },
-    { title: "Sign Out", action: signout, icon: "signout" },
   ];
 
   const unauthorizedLinks: ListItemType[] = [
     { title: "Home", path: "/", icon: "home" },
+    { title: "Sign up", path: "/signup", icon: "signup" },
+    { title: "Sign in", path: "/signin", icon: "signin" },
+  ];
+
+  const authorizedDropdown: ListItemType[] = [
+    { title: "Saved", path: "/saved", icon: "bookmarks" },
+    { title: "Settings", path: "/settings", icon: "settings" },
+    {
+      title: "Sign Out",
+      action: () => dispatch(authSignOut()),
+      icon: "signout",
+    },
+  ];
+
+  const unauthorizedDropdown: ListItemType[] = [
     { title: "Sign up", path: "/signup", icon: "signup" },
     { title: "Sign in", path: "/signin", icon: "signin" },
   ];
@@ -77,6 +148,9 @@ const MobileMenu = ({
             </li>
           )
         )}
+        <DropDownMenu
+          list={auth?.user ? authorizedDropdown : unauthorizedDropdown}
+        />
       </ul>
     </div>
   ) : null;
