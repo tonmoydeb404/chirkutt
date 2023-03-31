ce4bc19e0eae8edacc29dc90148c1be144c1b18a
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-03-31 23:30:15 +0600

    not found page edited

--- src/pages/error/NotFound.tsx
+++ src/pages/error/NotFound.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
@@ -6,7 +7,13 @@ const NotFound = () => {
       <Helmet>
         <title>Notfound - Chirkutt</title>
       </Helmet>
-      <div>NotFound</div>
+      <div className="flex w-full min-h-screen flex-col items-center justify-center fixed top-0 left-0 z-[10000]">
+        <h2 className="text-5xl font-bold">404</h2>
+        <p className="font-medium text-lg uppercase">page not found</p>
+        <Link to={"/"} className="btn btn-primary mt-10">
+          Back to home
+        </Link>
+      </div>
     </>
   );
 };
