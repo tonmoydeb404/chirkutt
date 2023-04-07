939f9ae0b3af3d05897a63160080a68d675fbd99
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-04-07 23:18:04 +0600

    post card actions updated with headless ui

--- src/common/components/post/card/PostCardActions.tsx
+++ src/common/components/post/card/PostCardActions.tsx
@@ -1,3 +1,5 @@
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 import { toast } from "react-hot-toast";
 import { useRemovePostNotificationsMutation } from "../../../../api/notificationsApi";
 import { useDeletePostMutation } from "../../../../api/postsApi";
@@ -30,22 +32,43 @@ const PostCardActions = ({ post }: PostCardActionsProps) => {
 
   return (
     <div className="flex items-center ml-auto gap-1">
-      <label className="dropdown">
-        <input type="checkbox" id={`drop-${post.id}`} />
-        <label
-          htmlFor={`drop-${post.id}`}
-          className="btn-icon btn-sm btn-ghost"
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="btn btn-sm btn-icon btn-theme">
+            {iconList.more}
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
         >
-          {iconList.more}
-        </label>
-
-        <ul>
-          <li onClick={handleEdit}>Edit Post</li>
-          <li className="text-error-600" onClick={handleDelete}>
-            Delete Post
-          </li>
-        </ul>
-      </label>
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right bg-white dark:bg-neutral-700 dark:text-white px-2 py-2 flex flex-col items-stretch gap-1 rounded border border-neutral-400 dark:border-neutral-600 shadow">
+            <Menu.Item>
+              <button
+                className="px-2 py-1 hover:bg-neutral-300 dark:hover:bg-neutral-600 text-left rounded flex items-center gap-1"
+                onClick={handleEdit}
+              >
+                <span>{iconList.pencil}</span>
+                Edit
+              </button>
+            </Menu.Item>
+            <Menu.Item>
+              <button
+                className="px-2 py-1 hover:bg-neutral-300 dark:hover:bg-neutral-600 text-left rounded flex items-center gap-1"
+                onClick={handleDelete}
+              >
+                <span>{iconList.remove}</span>
+                Delete
+              </button>
+            </Menu.Item>
+          </Menu.Items>
+        </Transition>
+      </Menu>
     </div>
   );
 };
