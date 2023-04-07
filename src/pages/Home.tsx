b8f96a644bff3d71f1e5beb2a7879fecdae300e5
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-04-07 23:29:05 +0600

    home page rendering changed & title updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import PostCard from "../common/components/post/card/PostCard";
 import PostCardSekeleton from "../common/components/skeletons/PostCardSkeleton";
 import usePosts from "../common/hooks/usePosts";
@@ -5,29 +6,30 @@ import usePosts from "../common/hooks/usePosts";
 const Home = () => {
   const { posts, isLoading, isError, error } = usePosts();
 
-  if (isError) {
-    return <p>something wents to wrong</p>;
-  }
-
-  if (posts instanceof Array && !isLoading) {
-    return (
-      <div className="flex flex-col gap-3">
-        {posts.length
-          ? posts?.map((post) => {
-              return <PostCard key={post.content.id} {...post} />;
-            })
-          : "nothing is here"}
-      </div>
-    );
-  }
-
   return (
-    <div className="flex flex-col gap-3">
-      <PostCardSekeleton />
-      <PostCardSekeleton />
-      <PostCardSekeleton />
-      <PostCardSekeleton />
-    </div>
+    <>
+      <Helmet>
+        <title>Home - Chirkutt</title>
+      </Helmet>
+      {!isLoading && !isError && posts instanceof Array ? (
+        <div className="flex flex-col gap-3">
+          {posts.length
+            ? posts?.map((post) => {
+                return <PostCard key={post.content.id} {...post} />;
+              })
+            : "nothing is here"}
+        </div>
+      ) : null}
+      {isError ? <p>something wents to wrong</p> : null}
+      {isLoading ? (
+        <div className="flex flex-col gap-3">
+          <PostCardSekeleton />
+          <PostCardSekeleton />
+          <PostCardSekeleton />
+          <PostCardSekeleton />
+        </div>
+      ) : null}
+    </>
   );
 };
 
